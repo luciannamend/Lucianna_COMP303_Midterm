7af62f29d954a7b73ac7f8a22d3dca381a116a4b
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-15 20:42:15 -0400

    fixed display sensors method

--- src/main/java/com/lucianna/mendonca/lucianna_comp303_midterm/controller/SensorController.java
+++ src/main/java/com/lucianna/mendonca/lucianna_comp303_midterm/controller/SensorController.java
@@ -2,11 +2,8 @@ package com.lucianna.mendonca.lucianna_comp303_midterm.controller;
 
 import com.lucianna.mendonca.lucianna_comp303_midterm.model.Sensor;
 import com.lucianna.mendonca.lucianna_comp303_midterm.repository.SensorRepository;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
