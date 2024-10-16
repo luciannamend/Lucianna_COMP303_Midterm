dcb92864ddac383f87349680483a6926154952ae
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-15 20:15:14 -0400

    Initial commit

---
+++ src/main/java/com/lucianna/mendonca/lucianna_comp303_midterm/LuciannaComp303MidtermApplication.java
@@ -0,0 +1,13 @@
+package com.lucianna.mendonca.lucianna_comp303_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuciannaComp303MidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuciannaComp303MidtermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucianna/mendonca/lucianna_comp303_midterm/LuciannaComp303MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianna.mendonca.lucianna_comp303_midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuciannaComp303MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
