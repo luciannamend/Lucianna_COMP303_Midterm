15a5e7b5d272c0b07c3c81077b15f743393f3b81
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-15 20:16:36 -0400

    Added sensor controller to manage crud methods

---
+++ src/main/java/com/lucianna/mendonca/lucianna_comp303_midterm/controller/SensorController.java
@@ -0,0 +1,64 @@
+package com.lucianna.mendonca.lucianna_comp303_midterm.controller;
+
+import com.lucianna.mendonca.lucianna_comp303_midterm.model.Sensor;
+import com.lucianna.mendonca.lucianna_comp303_midterm.repository.SensorRepository;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+public class SensorController {
+
+    private SensorRepository sensorRepository;
+
+    public SensorController(SensorRepository sensorRepository) {
+        this.sensorRepository = sensorRepository;
+    }
+
+    @GetMapping("/")
+    public String showMainPage(){
+        return "index";
+    }
+
+    @GetMapping("/sensorForm")
+    public String goToForm(){
+        return "index";
+    }
+
+    @PostMapping("/addSensor")
+    public String addSensor(Sensor sensor, RedirectAttributes redirectAttributes){
+        try{
+            // save sensor
+            sensorRepository.save(sensor);
+            redirectAttributes.addFlashAttribute(sensor);
+            return "showResult";
+        }
+        catch (Exception exception){
+            System.out.println(exception);
+            return "error";
+        }
+    }
+
+    @GetMapping("/displaySensorList")
+    public String displaySensorList(Model model){
+        List<Sensor> sensorList = sensorRepository.findAll();
+
+        if(sensorList.isEmpty()){
+            return "error";
+        }
+
+        int totalSensors = sensorList.size();
+
+        model.addAttribute("sensorList", sensorList);
+        model.addAttribute("totalSensors", totalSensors);
+
+        return "showSensorList";
+    }
+}
